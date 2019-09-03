9b0170f09ef65a7dae2da67c7af2e437e99ed01f
RevelesD <grass.dweller@gmail.com>
2019-09-03 16:28:14 -0500

    delete modules

--- dist/graphql/schemas/mutations.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mutations = `
-  createDocument(input: InputDocument!): Document!
-`;
-//# sourceMappingURL=mutations.js.map
--- dist/graphql/schemas/queries.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.queries = `
-  getDocument(id: ID!): Document!
-  getDocuments(limit: Int!): [Document!]!
-`;
-//# sourceMappingURL=queries.js.map
