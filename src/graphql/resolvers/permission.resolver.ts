234ff8b97cf450d008a8f00b79085c3702e2161a
John Reb <john.reb97@gmail.com>
2019-11-05 15:40:20 -0600

    Update permission.resolver.ts

--- src/graphql/resolvers/permission.resolver.ts
+++ src/graphql/resolvers/permission.resolver.ts
@@ -27,7 +27,7 @@ const permissionQueries = {
     const qType = 'Query';
     const qName = 'permissions';
     try {
-      if (!await isAuth(context, [config.permission.admin])) {
+      if (!await isAuth(context, [config.permission.superAdmin])) {
         const error = registerBadLog(context, qType, qName);
         throw new ApolloError(`S5, Message: ${error}`);
       }
